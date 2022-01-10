80ec6d4e75ef753d6318d1d18ceccbc387ed8e35
Zannatul Naim <97250227+Zannatul-Naim@users.noreply.github.com>
2022-01-10 18:05:18 +0600

    Program to swap two numbers

---
+++ PYTHON/SwapNumbers.py
@@ -0,0 +1,16 @@
+
+# Program to swap two numbers without using temporary variable
+
+x = 5
+y = 10
+
+print("Before swapping: \n")
+print("x = " +  str(x))
+print("y = " + str(y))
+
+#swap here
+x, y = y, x
+
+print("\nAfter swapping: \n")
+print("x = " +  str(x))
+print("y = " + str(y))
