1c6f52744c4deda0420473cc39adbdb47d9cdf58
Zannatul Naim <97250227+Zannatul-Naim@users.noreply.github.com>
2022-01-12 07:00:54 +0600

    Python comment

---
+++ PYTHON/Basic/Comments.py
@@ -0,0 +1,6 @@
+#This is a Single line comment.
+print("Hello, World!")
+
+# python doesn't support multi line comment
+# In python alternative of multiline comment is docstring that's written as (""") and closing (""").
+
