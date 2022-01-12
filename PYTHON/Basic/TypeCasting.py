d4905feb91bcf80ff7e896172059bc3647d6356c
Zannatul Naim <97250227+Zannatul-Naim@users.noreply.github.com>
2022-01-12 07:09:58 +0600

    Type Casting in python

---
+++ PYTHON/Basic/TypeCasting.py
@@ -0,0 +1,28 @@
+# Integer casting
+
+x = int(1)  # Declared a variable x and casting a value 1 as integer to it.
+y = int(2.8) # Declared a variable x and casting a value 1 as integer to it.
+z = int("3")  # Declared a variable z and casting a string "3" as integer to it.
+print(x)  # print x
+print(y)  # pring y
+print(z)  # print z
+
+# Floating point casting
+
+x = float(1)  # Declared a variable x and casting a value 1 as double to it.
+y = float(2.8)  # Declared a variable y and casting a value 2.8 as double to it.
+z = float("3")  # Declared a variable z and casting a string "3" as double to it.
+w = float("4.2")  # Declared a variable w and casting a string "4.2" as double to it.
+print(x)
+print(y)
+print(z)
+print(w)
+
+# String casting
+
+x = str("s1")
+y = str(2)
+z = str(3.0)
+print(x)
+print(y)
+print(z)
