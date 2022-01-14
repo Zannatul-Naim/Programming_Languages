42c45f87825d9f643ea1bdadf94538050e73475a
Zannatul Naim <97250227+Zannatul-Naim@users.noreply.github.com>
2022-01-14 20:42:53 +0600

    Ascii value of Alphabet.

---
+++ JAVA/Basic/AlphabetASCIIValue.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class AlphabetASCIIValue {
+
+   public static void main(String[] args) {
+
+      // declare a char variable
+      char ch ;
+      // declare a int variable
+      int value ;
+      
+      Scanner scan = new Scanner(System.in);
+
+      // read alphabet
+      System.out.print("Enter an alphabet: ");
+      ch = scan.next().charAt(0);
+
+      // convert char to ascii value
+      value = ch;
+
+      // display ascii value
+      System.out.println("ASCII value of "+
+                     ch+" = "+value);
+ 
+      // we can directly display ASCII value
+      // using type-casting (int)ch
+      System.out.println("ASCII value of "+
+                 ch+" = "+ (int)ch );
+
+      // close Scanner class object
+      scan.close();
+     }
+ }
