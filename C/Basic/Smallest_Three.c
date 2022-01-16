e3bbfb3c8e79d42d69dacc806b68dc38cdc112d9
Zannatul Naim <97250227+Zannatul-Naim@users.noreply.github.com>
2022-01-16 19:29:51 +0600

    Smallest value among three integers.

---
+++ C/Basic/Smallest_Three.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+int main()
+{
+    int a, b, c, small;
+    printf("Enter three numbers : ");
+    scanf("%d %d %d", &a, &b, &c);
+    small = (a < b) ? (a < c ? a : c) : (b < c ? b : c);   // for finding the smallest value.
+    printf("\nSmallest value is = %d", small);
+    return 0;
+}
