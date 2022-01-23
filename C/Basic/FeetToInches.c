efdaa8256247901dda3c26f58d0e4c49f7204cd2
Zannatul Naim <97250227+Zannatul-Naim@users.noreply.github.com>
2022-01-23 12:54:10 +0600

    Convert feet to inches.

---
+++ C/Basic/FeetToInches.c
@@ -0,0 +1,17 @@
+/*  C program to convert feet to inches  */
+
+#include<stdio.h>
+
+int main()
+{
+                int feet, inches;
+                
+                printf("Enter the value of feet: ");
+                scanf("%d", &feet);
+                
+                //converting into inches
+                inches = feet * 12;
+                
+                printf("\nTotal inches will be: %d\n", inches);
+                return 0;
+}
