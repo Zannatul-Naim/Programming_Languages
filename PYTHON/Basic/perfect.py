f80e58320c03550e978da4ff7a0e46a9d9c9757e
Zannatul Naim <97250227+Zannatul-Naim@users.noreply.github.com>
2022-01-24 19:39:35 +0600

    Perfect number.

---
+++ PYTHON/Basic/perfect.py
@@ -0,0 +1,12 @@
+# Chcek weather a number perfect or not. 
+
+print("Enter the Number:")
+num = int(input())
+sum = 0
+for i in range(1, num):
+  if num%i==0:
+    sum = sum+i
+if num==sum:
+  print("It is a Perfect Number")
+else:
+  print("It is not a Perfect Number")
