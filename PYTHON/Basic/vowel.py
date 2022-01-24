aacd79bcc169ecab2afab26eefe11446a533a02a
Zannatul Naim <97250227+Zannatul-Naim@users.noreply.github.com>
2022-01-24 19:34:53 +0600

    Check a character vowel or not .

---
+++ PYTHON/Basic/vowel.py
@@ -0,0 +1,11 @@
+#check a character vowel or not. 
+
+print("Enter the Character: ")
+c = input()
+
+if c=='a' or c=='e' or c=='i' or c=='o' or c=='u':
+    print("\nIt is a Vowel")
+elif c=='A' or c=='E' or c=='I' or c=='O' or c=='U':
+    print("\nIt is a Vowel")
+else:
+    print("\nIt is a Consonant")
