3a35bc2f9d504310b5da429ff198b9c77712bc09
Zannatul Naim <97250227+Zannatul-Naim@users.noreply.github.com>
2022-01-27 19:52:44 +0600

    Using (AND) && Operator.

--- C/Basic/largestThree.c
+++ C/Basic/largestThree.c
@@ -1,28 +1,17 @@
 #include<stdio.h>
 int main()
 {
-   float num1, num2, num3;
+   int num1, num2, num3;
 
    printf("Enter three numbers: ");
-   scanf("%f %f %f",&num1,&num2,&num3);
+   scanf("%d %d %d",&num1, &num2, &num3);
 
-   if(num1 >= num2)
-   {
-     if(num1 >= num3)
-       printf("Largest Number=%.2f\n",num1);
-
-     else
-       printf("Largest Number=%.2f\n",num3);
-   }
-
+   if(num1 >= num2 && num1 >= num3)
+     printf("Largest = %d\n", num1);
+   else if(num2 >= num1 && num2 >= num3)
+     printf("Largest = %d\n", num2);
    else
-   {
-     if(num2>=num3)
-       printf("Largest Number=%.2f\n",num2);
-
-     else
-       printf("Largest Number=%.2f\n",num3);
-   }
+     printf("Largest = %d\n", num3);
 
    return 0;
 }
