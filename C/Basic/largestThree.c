32a25bd7994f101e29c1474904451aaa5f74546e
Zannatul Naim <97250227+Zannatul-Naim@users.noreply.github.com>
2022-01-27 19:53:44 +0600

    Find Largest using another variable max.

--- C/Basic/largestThree.c
+++ C/Basic/largestThree.c
@@ -1,17 +1,16 @@
-#include<stdio.h>
-int main()
-{
-   int num1, num2, num3;
+ #include<stdio.h>
+ int main()
+ {
+   int num1, num2, num3, max;
 
    printf("Enter three numbers: ");
-   scanf("%d %d %d",&num1, &num2, &num3);
+   scanf("%d %d %d",&num1,&num2,&num3);
 
-   if(num1 >= num2 && num1 >= num3)
-     printf("Largest = %d\n", num1);
-   else if(num2 >= num1 && num2 >= num3)
-     printf("Largest = %d\n", num2);
-   else
-     printf("Largest = %d\n", num3);
+   max = num1;
+   if(num2>max) max = num2;
+   if(num3>max) max = num3;
+
+   printf("Largest number = %d", max);
 
    return 0;
-}
+ }
