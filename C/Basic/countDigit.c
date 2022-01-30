883de2fddd72849f00fe27f456e3fc3b1e35ff7f
Zannatul Naim <97250227+Zannatul-Naim@users.noreply.github.com>
2022-01-30 21:59:49 +0600

    (Updated) Count Digit using recursion.

--- C/Basic/countDigit.c
+++ C/Basic/countDigit.c
@@ -1,4 +1,6 @@
- #include<stdio.h>
+/*
+
+#include<stdio.h>
  int main()
  {
      int number, count = 0;
@@ -13,6 +15,28 @@
      }
 
      printf("Number of digits =  %d\n", count);
+
+     return 0;
+ }
+
+
+*/
+
+ #include<stdio.h>
+ int numberOfDigits(int n)
+ {
+   return n==0? 0 : 1+numberOfDigits(n/10);
+ }
+ int main()
+ {
+     int number, sum;
+
+     printf("Enter a number: ");
+     scanf("%d",&number);
+
+     sum = numberOfDigits(number);
+
+     printf("Number of digits =  %d\n",sum);
 
      return 0;
  }
