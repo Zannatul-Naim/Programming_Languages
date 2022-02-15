9db1c6fc5c2ca1b00097ed816063304d640f67fa
Zannatul Naim <97250227+Zannatul-Naim@users.noreply.github.com>
2022-02-15 14:05:52 +0600

    Creating a set

---
+++ PYTHON/Basic/Set/createSet.py
@@ -0,0 +1,8 @@
+sampleSet = {"apple", "banana", "cherry"}
+print(sampleSet)
+
+# set doesn't take any duplicate number.
+
+# Note: the set list is unordered, meaning: the items will appear in a random order.
+
+# Refresh this page to see the change in the result.
