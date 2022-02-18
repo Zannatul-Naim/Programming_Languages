0a9f8a647fc83a9e4af98fcb863602295bea81d3
Zannatul Naim <97250227+Zannatul-Naim@users.noreply.github.com>
2022-02-18 21:31:32 +0600

    Find length of a string

---
+++ JAVA/String/LengthOfString.java
@@ -0,0 +1,6 @@
+public class LengthOfString {
+  public static void main(String[] args) {
+    String txt = "My gold bangla I love you!";
+    System.out.println("The length of the txt string is: " + txt.length());
+  }
+}
