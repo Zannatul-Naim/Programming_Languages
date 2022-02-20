312596ab8b9d9063ea980290e8c5b49cafedfe2e
Zannatul Naim <97250227+Zannatul-Naim@users.noreply.github.com>
2022-02-20 18:22:20 +0600

    Create function and call it

---
+++ C++/Basic/Function/function.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+void myFunction() {   // myFunction is created
+  cout << "I just got executed!";
+}
+
+int main() {
+  myFunction();   // myFunction is called
+  return 0;
+}
