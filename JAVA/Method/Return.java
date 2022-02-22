c187463e3bde8eebbdb8d790c24a16b6b4fdc206
Zannatul Naim <97250227+Zannatul-Naim@users.noreply.github.com>
2022-02-22 21:14:02 +0600

    Working of Return in  Method

---
+++ JAVA/Method/Return.java
@@ -0,0 +1,10 @@
+public class Return {
+  static int myMethod(int x) {
+    return x * x;   // Returning Square of a number
+  }
+
+  public static void main(String[] args) {
+    
+    System.out.println(myMethod(3));
+  }
+}
