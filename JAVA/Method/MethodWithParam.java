bd2293c7d30d2ddca4a1cfc78d9a34118ac5bc16
Zannatul Naim <97250227+Zannatul-Naim@users.noreply.github.com>
2022-02-22 21:04:58 +0600

    Method with Parameter

---
+++ JAVA/Method/MethodWithParam.java
@@ -0,0 +1,14 @@
+public class MethodWithParam {
+  static void myMethod(String name) {
+    
+    System.out.println(fname + " Refsnes");
+  }
+
+  public static void main(String[] args) {
+    
+    myMethod("Liam");
+    myMethod("Jenny");
+    myMethod("Anja");
+    
+  }
+}
