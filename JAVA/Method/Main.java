eb8b28de8bd0cb012824a736ef2e0f0d696c50c0
Zannatul Naim <97250227+Zannatul-Naim@users.noreply.github.com>
2022-02-22 21:01:58 +0600

    Create a Method

---
+++ JAVA/Method/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+  static void myMethod() {
+  // inside the method
+  
+    System.out.println("Java method Executing");
+  }
+
+  public static void main(String[] args) {
+    myMethod();  // Calling the method
+  }
+}
