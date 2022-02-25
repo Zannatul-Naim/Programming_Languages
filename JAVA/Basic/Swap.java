16642d7351b9a89a2a98bec8d74746312b634f6a
Zannatul Naim <97250227+Zannatul-Naim@users.noreply.github.com>
2022-02-25 11:29:06 +0600

    Swap two numbers

--- JAVA/Basic/Swap.java
+++ JAVA/Basic/Swap.java
@@ -7,11 +7,15 @@ public class Swap {
   System.out.println("Values Before Swapping,");
 	System.out.println("x = " + x + "\t y = " + y);
     
-	int temp;
-  // swapping two numbers using temp variable. 
-	temp = x;
-	x = y;
-	y = temp;
+// 	int temp;
+//   // swapping two numbers using temp variable. 
+// 	temp = x;
+// 	x = y;
+// 	y = temp;
+	// Without using temp varible.
+	x = x + y;
+	y = x - y;
+	x = x - y;
 
 	System.out.println("Values After Swapping,");
 	System.out.println("x = " + x + "\t y = " + y);
