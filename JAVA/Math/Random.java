1548e340e5eebd28ee3c5c4b7e579bba32300b2c
Zannatul Naim <97250227+Zannatul-Naim@users.noreply.github.com>
2022-02-25 23:34:04 +0600

    Random number

--- JAVA/Math/Random.java
+++ JAVA/Math/Random.java
@@ -1,6 +1,7 @@
 public class Main {
   public static void main(String[] args) {
-    
-    System.out.println(Math.random());  
+    // To find random numbers between 0 to 100
+    // Use Math.random() method to obtain this
+    System.out.println(Math.random() * 100);  
   }
 }
