87d229c38e05e55ab7e257f0a07e796eafa21612
Zannatul Naim <97250227+Zannatul-Naim@users.noreply.github.com>
2022-02-25 23:29:36 +0600

    Find maximum

--- JAVA/Math/MaxValue.java
+++ JAVA/Math/MaxValue.java
@@ -1,6 +1,8 @@
-public class MMaxValue {
+public class MaxValue {
   public static void main(String[] args) {
-    
-    System.out.println(Math.max(5, 10));  
+    int a = 10;
+    int b = 20;
+    // Use Math.max() method to find maximum value
+    System.out.println(Math.max(a, b));  
   }
 }
