49491f3f11caca03581cbbba1727c92c9915c857
Zannatul Naim <97250227+Zannatul-Naim@users.noreply.github.com>
2022-02-25 23:30:43 +0600

    Minimum value

--- JAVA/Math/MinValue.java
+++ JAVA/Math/MinValue.java
@@ -1,5 +1,9 @@
 public class MinValue {
   public static void main(String[] args) {
-    System.out.println(Math.min(5, 10));  
+    int a, b;
+    a = 10;
+    b = 100;
+    // Use Math.min() method to find minimum value between two numbers
+    System.out.println(Math.min(a, b));  
   }
 }
