f72b7f93100a54b17c8ed2b9ca74716811d2cceb
Zannatul Naim <97250227+Zannatul-Naim@users.noreply.github.com>
2022-03-02 23:38:01 +0600

    Find the Compound Interest

--- JAVA/Basic/CompoundInterest.java
+++ JAVA/Basic/CompoundInterest.java
@@ -3,15 +3,13 @@ import java.util.Scanner;
 public class CompoundInterest {
    public static void main(String[] args) {
 
-       // declare variables
        double principal = 0.0, rate = 0.0, time = 0.0;
        int number = 0;
        double totalAmount = 0.0, cinterest = 0.0;
-
-       // create object of Scanner class
+      
        Scanner scan = new Scanner(System.in);
 
-       // read inputs
+      
        System.out.print("Enter principal amount:: ");
        principal = scan.nextDouble();
        System.out.print("Enter the annual compound interest rate :: ");
@@ -19,13 +17,11 @@ public class CompoundInterest {
        rate = rate/100; // convert rate
        System.out.print("Enter time (in years):: ");
        time = scan.nextDouble();
-       System.out.print("Enter the number of times that "+
-                    +"interest is compounded per year:: ");
+       System.out.print("Enter the number of times that " + "interest is compounded per year:: ");
        number = scan.nextInt();
 
        // calculate total amount
-       totalAmount = principal * 
-                 Math.pow( 1+(rate/number), number*time);
+       totalAmount = principal * Math.pow( 1+(rate/number), number*time);
 
        // calculate compound interest
        cinterest = totalAmount - principal;
