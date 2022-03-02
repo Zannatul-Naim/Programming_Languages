93c4ca690ef27cd7d1ebccec29f9621aea81d26f
Zannatul Naim <97250227+Zannatul-Naim@users.noreply.github.com>
2022-03-02 23:33:47 +0600

    Sum of all the elements in the array

--- JAVA/Array/ArraySum.java
+++ JAVA/Array/ArraySum.java
@@ -1,6 +1,5 @@
 public class ArraySum {
 
-    // main method
     public static void main(String[] args) {
       // array with explicit values
       int[] array = {5,15,25,35,45,55,65};
@@ -9,12 +8,10 @@ public class ArraySum {
       System.out.println("\nSum of array elements: " + sum);
     }
   
-    // method to display array elements
-    // and calculate sum
     static int sumOfArrayElements(int[] a) {
-      int sum=0;
+      int sum = 0;
       // display array and calculate sum
-      for(int i=0; i< a.length; i++){
+      for(int i = 0; i < a.length; i++){
         System.out.print(a[i]+"\t");
         sum += a[i];
       }
