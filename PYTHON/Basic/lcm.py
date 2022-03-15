f855edeb57f7c45b0e06463e1125020b4e8bcad0
Zannatul Naim <97250227+Zannatul-Naim@users.noreply.github.com>
2022-03-15 23:30:52 +0600

    Find LCM of two numbers

--- PYTHON/Basic/lcm.py
+++ PYTHON/Basic/lcm.py
@@ -1,10 +1,10 @@
-a=int(input("Enter the first number:"))
-b=int(input("Enter the second number:"))
+a=int(input("Enter the first number: "))
+b=int(input("Enter the second number: "))
 if(a>b):
     min1=a
 else:
     min1=b
-while(1):
+while(True):
     if(min1%a==0 and min1%b==0):
         print("LCM is:",min1)
         break
